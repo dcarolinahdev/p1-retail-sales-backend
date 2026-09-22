633a15678dd1ff44b153dd3f95ea6292c2e435e9
Diana Carolina Hernandez Abahonza <dcarolinahdev@gmail.com>
2026-09-22 16:00:30 -0500

    feat: add Prisma module and configure environment variables loading

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { PrismaModule } from './prisma/prisma.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      /* hace que ConfigModule cargue el .env una sola vez al iniciar y
+       * esas variables queden disponibles para toda la aplicación 
+       * (incluyendo PrismaService, que las necesita para conectarse).
+      */
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
