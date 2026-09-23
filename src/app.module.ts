06e7efcbda81b05264364bb12c41d7ff682388f5
Diana Carolina Hernandez Abahonza <dcarolinahdev@gmail.com>
2026-09-23 12:16:42 -0500

    feat: implement Clientes CRUD module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { PrismaModule } from './prisma/prisma.module';
-
 import { AuthModule } from './auth/auth.module';
+import { ClientesModule } from './clientes/clientes.module';
 
 @Module({
   imports: [
@@ -19,6 +19,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     PrismaModule,
     AuthModule,
+    ClientesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
